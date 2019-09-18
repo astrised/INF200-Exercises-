723b24459d8f76592c8fd880566951db6db529d7
Astrid <astrised@nmbu.no>
2019-09-18 13:10:30 +0200

    Finished task A

--- src/astrid_sedal_ex/ex01/deck_comp.py
+++ src/astrid_sedal_ex/ex01/deck_comp.py
@@ -1,7 +1,3 @@
-Exersice 1
-----------
-
-
 SUITS = ('C', 'S', 'H', 'D')
 VALUES = range(1, 14)
 
@@ -12,8 +8,9 @@ def deck_loop():
             deck.append((suit, val))
     return deck
 
+
 def deck_comp():
-    return deck = [(suit, val) for suit in SUITS for val in VALUES]
+    return [(suit, val) for suit in SUITS for val in VALUES]
 
 
 if __name__ == '__main__':
