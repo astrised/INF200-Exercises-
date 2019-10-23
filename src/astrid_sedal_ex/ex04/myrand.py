d76409f04f7675093a8b3584bd2b9cef25906f87
Astrid <astrised@nmbu.no>
2019-10-23 20:49:20 +0200

    Updated myrand, walker and passed tests in test_ex04

--- src/astrid_sedal_ex/ex04/myrand.py
+++ src/astrid_sedal_ex/ex04/myrand.py
@@ -7,17 +7,37 @@ import random
 
 
 class LCGRand:
-    def __init__(self):
-        pass
+    def __init__(self, seed):
+        self.r_n = seed
+        self.a = 7**5
+        self.m = 2**31 - 1
 
     def rand(self):
-       pass
+        self.r_n = self.a * self.r_n % self.m
+        return self.r_n
 
 
 class ListRand:
-    def __init__(self):
-        pass
+    def __init__(self, numbers):
+        self.numbers = numbers
+        self.current_idx = -1
+        self.n = len(numbers)
 
     def rand(self):
-        pass
+        self.current_idx += 1
 
+        if self.current_idx >= self.n:
+            raise RuntimeError('Index out of range')
+        return self.numbers[self.current_idx]
+
+
+if __name__ == "__main__":
+    lcg = LCGRand(346)
+    print(lcg.rand())
+    print(lcg.rand())
+
+    numbers = [1, 7]
+    lr = ListRand(numbers)
+    print(lr.rand())
+    print(lr.rand())
+    print(lr.rand())
