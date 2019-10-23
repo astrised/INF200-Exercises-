c82f65a2829967978a80276a480098564a85d2c6
Astrid <astrised@nmbu.no>
2019-10-23 13:33:16 +0200

    Updated myrand

---
+++ src/astrid_sedal_ex/ex04/myrand.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Astrid Hæve Sedal'
+__email__ = 'astrised@nmbu.no'
+
+import random
+
+
+class LCGRand:
+    def __init__(self):
+        pass
+
+    def rand(self):
+       pass
+
+
+class ListRand:
+    def __init__(self):
+        pass
+
+    def rand(self):
+        pass
+
